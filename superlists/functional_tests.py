ddb16aba99abdf1326c91a504351ffd5a6821718
hailxo <hailxl@gmail.com>
2017-11-15 22:43:11 +0800

    add test case for buy peacock feathers

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -26,8 +26,8 @@ class NewVisitorTest(unittest.TestCase):
 
         # 应用邀请她输入一个待办事项
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEquals(inputbox.get_attribute('placeholder'),
-                          'Enter a to-do item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                         'Enter a to-do item')
 
         # 她在一个文本框中输入了“Buy peacock feathers”（购买孔雀羽毛）
         # 伊迪丝的爱好是使用假蝇做饵钓鱼
@@ -42,22 +42,33 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn('1: Buy peacock feathers',
-                            [row.text for row in rows])
+                      [row.text for row in rows])
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
         # 伊迪丝做事很有条理
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 页面再次更新，她的清单中显示了这两个待办事项
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
+
+        # 伊迪丝想知道这个网站是否会记住她的清单
+
+        # 她看到网站为她生成了一个唯一的 URL
+        # 而且页面中有一些文字解说这个功能
+
+        # 她访问那个 URL，发现她的待办事项列表还在
+
         self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
     unittest.main()
-
-# 页面再次更新，她的清单中显示了这两个待办事项
-
-# 伊迪丝想知道这个网站是否会记住她的清单
-
-# 她看到网站为她生成了一个唯一的 URL
-# 而且页面中有一些文字解说这个功能
-
-# 她访问那个 URL，发现她的待办事项列表还在
