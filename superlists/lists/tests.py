de9724fae02c39dc983fd2bc4e2ece583fa8421e
hailxo <hailxl@gmail.com>
2017-12-08 08:22:39 +0800

    新增用于新建清单的 url 和视图

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -14,11 +14,9 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
         response = home_page(request)
         ## nothing to test
-        print(repr(response))
+        # print(repr(response))
         # self.assertIn('A new list item', response.content.decode())
         # expect_html = render_to_string(
         #     'home.html',
@@ -27,34 +25,6 @@ class HomePageTest(TestCase):
         # print("response:", response.content.decode())
         # FIXME 含隐藏表单导致测试失败
         # self.assertEqual(response.content.decode(), expect_html)
-
-    def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-
-        home_page(request)
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-
-        response = home_page(request)
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'],
-                         '/lists/the-only-list-in-the-world/')
-
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ItemModleTest(TestCase):
 
@@ -90,3 +60,23 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'itemy 1')
         self.assertContains(response, 'itemy 2')
+
+
+class NewListTest(TestCase):
+
+    def test_saveing_a_POST_request(self):
+        self.client.post('/lists/new',
+                         data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new',
+                                    data={'item_text': 'A new list item'})
+
+        self.assertEqual(response.status_code, 302)
+        ## 新版本 django 重定向后的 url 并没有包含 host
+        self.assertEqual(response['location'],
+                         '/lists/the-only-list-in-the-world/')
